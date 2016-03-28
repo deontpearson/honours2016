5cc57e16c53a86a4e7ee7b65d20b10d036f527e0
Deon Pearson <deontpearson@gmail.com>
2016-03-28 08:50:57 +0200

    stuff

--- my_honeypot/honeypot.py
+++ my_honeypot/honeypot.py
@@ -8,7 +8,14 @@ def index():
     return "home page", 200 #app.send_static_file('index.html'), 200 # returns the text Hello Flask and the response code 200
 @app.route("/index")
 def adminPage():
-    return render_template('index.html')
+    return "index page", 200
+@app.route("/wp-admin")
+def wordPressAdmin():
+    return "word press admin page", 200
 if __name__ == "__main__": # __name__ is the name attirbute of the objects
     app.debug = True # enable debugging so that code can be changed without restarting the server
     app.run(host="146.231.123.9", port=8080)
+    logger = logging.getLogger('wekzeug') # gets the underlying WSGI logger
+    handler = logging.FileHandler('requests.log')
+    logger.addHandler(handler) # adds the handler instance to the logger
+    app.logger.addhandler(handler)
