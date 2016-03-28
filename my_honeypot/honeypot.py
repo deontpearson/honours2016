fee5bc51589343f8cc3a4032a7271647924bac66
Deon Pearson <deontpearson@gmail.com>
2016-03-28 08:34:17 +0200

    changes

--- my_honeypot/honeypot.py
+++ my_honeypot/honeypot.py
@@ -8,18 +8,15 @@ app = Flask(__name__, static_url_path='') # creates an instance of the Flask cla
 
 @app.route('/') # defines what must be done when a request comes in for "/"
 def index():
-    app.logger.info(request.remote_addr + 'blaaaaa')
+    print (request.method)
     return "home page", 200 #app.send_static_file('index.html'), 200 # returns the text Hello Flask and the response code 200
 @app.route("/index")
 def adminPage():
     return render_template('index.html')
 if __name__ == "__main__": # __name__ is the name attirbute of the objects
-    app.debug = True # enable debugging so that code can be changed without restarting the server
-    handler = RotatingFileHandler('acces.log', maxBytes=10000, backupCount=1)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-
-
-    app.run(host="127.0.0.1", port=8080)
-
+    app.debug = False # enable debugging so that code can be changed without restarting the server
+    #log = logging.getLogger('werkzeug')
+    #log.setLevel(logging.ERROR)
     #app.logger.addHandler(handler)
+    logging.basicConfig(filename='error.log', level=logging.ERROR)
+    app.run(host="127.0.0.1", port=8080)
