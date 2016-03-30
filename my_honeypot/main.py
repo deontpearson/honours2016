86eae0b8ffa4748dd0fab7ca5b6a883348bdf6e0
Deon Pearson <deontpearson@gmail.com>
2016-03-30 14:49:22 +0200

    1) added stats file and access logs. 2) implemented templating 3) implemented a catch all function for any path

--- my_honeypot/main.py
+++ my_honeypot/main.py
@@ -1,3 +1,10 @@
+#------------------------------------------------------------------------------------#
+#-------------------------------REQUIRED MODULES-------------------------------------#
+#------------------------------------------------------------------------------------#
+# pip install jinja2 > for templating
+#------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------#
 # usefull info drom other modules
 # request logger args = {
 #   'pid':primaryID,
@@ -9,28 +16,38 @@
 #   'code': responseCode
 # }
 
-
 from flask import Flask # imports the flask module to use in this app
 from flask import request # allows us to see the request headers
-from flask import render_template
-
-
 # Custom modules for my honeypot
 from modules.logger import my_logger
+from modules.handler import my_handler
 
-app = Flask(__name__, static_url_path='') # creates an instance of the Flask class and calls it app
+SPECIAL_PAGES = ['stats', 'requests']
 
-@app.route('/') # defines what must be done when a request comes in for "/"
-def index():
-    my_logger.logRequest(log, {'pid':1, 'src': request.remote_addr, 'method': request.method, 'resource': request.url, 'http': 'HTTP1/1', 'code': 200})
-    return "home page", 200 #app.send_static_file('index.html'), 200 # returns the text Hello Flask and the response code 200
-@app.route("/index")
-def adminPage():
-    return "index page", 200
-@app.route("/wp-admin")
-def wordPressAdmin():
-    return "word press admin page", 200
+app = Flask(__name__) # creates an instance of the Flask class and calls it app
+
+# With these routes setup we are catching *any* possible URL/request.
+# The root URL/domain for the app (/) is catched by the first route
+# While the second route using the path placeholder will catch any
+# route like /login, /logout, or even /foo/bar.
+@app.route('/', defaults={'path':'/'}) # defines what must be done when a request comes in for "/"
+@app.route('/<path:path>')
+def catchAll(path):
+    if path not in SPECIAL_PAGES:
+        my_logger.logRequest(log, {'src': request.remote_addr, 'method': request.method, 'resource': request.path, 'http': 'HTTP1/1', 'code': 200}) # logs every request that comes into the server
+    pathOptions = {'/':my_handler.rootHandler,
+                    'wp-admin':my_handler.wordPressAdminHandler,
+                    'stats': my_handler.displayStats,
+                    'requests' :  my_handler.displayRequestsInfo
+                    }
+
+    if path in SPECIAL_PAGES and not request.remote_addr == '146.231.123.9': # special path
+        return my_handler.noHandler(), 203
+    elif path in pathOptions: # handle any implemented modules
+        return pathOptions[path](), 200
+    else: # handle any paths not implemented
+        return my_handler.noHandler(path), 203
+
 if __name__ == "__main__": # __name__ is the name attirbute of the objects
-    #app.debug = True # enable debugging so that code can be changed without restarting the server
-    log = my_logger.getMyLogger() # creates a new log object to write logs to
-    app.run(host="127.0.0.1", port=8080)
+    log = my_logger.getMyLogger('request_logger') # creates a new log object to write logs to
+    app.run(host="146.231.123.9", port=8080,debug=True)
--- my_honeypot/modules/logger/my_logger.py
+++ my_honeypot/modules/logger/my_logger.py
@@ -1,16 +1,41 @@
 import logging.config # to use a config file for logging
 import logging # to use the standard pyton logger
-from datetime import datetime
+import datetime
 
-def getMyLogger():
+STATS = {}
+ADDRESS_COUNTER = {}
+
+def loadStats():
+    "retrievs stats from a file name stats.info"
+    global STATS
+    tempList = open('./data/stats.info', 'r').read().split('\n')
+    for i in tempList:
+        if i != '':
+            temp = i.split('=')
+            STATS[temp[0]] = temp[1]
+
+def saveStats():
+    "saves the stats from dictionary to a file in data/stats.info"
+    with open('./data/stats.info', 'w') as f:
+        for l in STATS:
+            f.write('%s=%d\n' % (l,STATS[l]))
+
+def getMyLogger(loggerName):
     "retunrs a log file to be used"
     logging.config.fileConfig('modules/logger/logging.conf') # uses an external logging file to get configuration information
-    return logging.getLogger('request_logger') # sets log to be the request logger from the logging.conf file
+    loadStats()
+    return logging.getLogger(loggerName) # sets log to be the request logger from the logging.conf file
 
 def logRequest(log, args):
     """takes in a log object and a ditionary of arguments
     args = {'pid':primaryID, 'src': soruceAddress, 'date':datetime, 'method':method, 'resource': resource, 'http': httpVersion, 'code': responseCode}
     """
+    global STATS, ADDRESS_COUNTER
     temp = args
-    temp['date'] = datetime.now()
+    format = '%d %b %H:%M:%S %Y'
+    today = datetime.datetime.today()
+    temp['date'] = today.strftime(format)
+    temp['pid'] = int(STATS['REQUESTS'])
     log.info('[%(pid)d] %(src)s - - [%(date)s] \"%(method)s %(resource)s %(http)s\" %(code)d'  % temp)
+    STATS['REQUESTS'] = int(STATS['REQUESTS']) + 1
+    saveStats()
